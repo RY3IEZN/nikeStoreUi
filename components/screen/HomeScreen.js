45bea6f6571aa5ffc3e35010040e8fba5199e02f
unknown <ghostlordwalker@gmail.com>
2022-04-05 04:18:33 +0100

    homescreen done

--- components/screen/HomeScreen.js
+++ components/screen/HomeScreen.js
@@ -9,6 +9,7 @@ import {
   Text,
   Image,
 } from "react-native";
+import { Svg, Polygond, Polygon } from "react-native-svg";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function HomeScreen(props) {
@@ -41,21 +42,73 @@ function HomeScreen(props) {
       sizes: [6, 7, 8, 9],
     },
   ]);
+  const [recentlyViewed, setRecentlyViewed] = useState([
+    {
+      id: 0,
+      name: "Nike Metcon 4",
+      img: require("../../assets/nike-metcon-4.png"),
+      bgColor: "#414045",
+      type: "TRAINING",
+      price: "$119",
+      sizes: [6, 7, 8],
+    },
+    {
+      id: 1,
+      name: "Nike Metcon 6",
+      img: require("../../assets/nike-metcon-6.png"),
+      bgColor: "#4EABA6",
+      type: "TRAINING",
+      price: "$135",
+      sizes: [6, 7, 8, 9, 10, 11],
+    },
+    {
+      id: 2,
+      name: "Nike Metcon 5",
+      img: require("../../assets/nike-metcon-5-purple.png"),
+      bgColor: "#2B4660",
+      type: "TRAINING",
+      price: "$124",
+      sizes: [6, 7, 8, 9],
+    },
+    {
+      id: 3,
+      name: "Nike Metcon 3",
+      img: require("../../assets/nike-metcon-3.png"),
+      bgColor: "#A69285",
+      type: "TRAINING",
+      price: "$99",
+      sizes: [6, 7, 8, 9, 10, 11, 12, 13],
+    },
+    {
+      id: 4,
+      name: "Nike Metcon Free",
+      img: require("../../assets/nike-metcon-free.png"),
+      bgColor: "#A02E41",
+      type: "TRAINING",
+      price: "$108",
+      sizes: [6, 7, 8, 9, 10, 11],
+    },
+  ]);
 
   const TrendingShoes = (item, index) => {
     return (
       <TouchableOpacity style={styles.trendshoeslist}>
-        <Text>{item.type}</Text>
+        <Text style={{ color: "grey", fontSize: 15, fontWeight: "700" }}>
+          {item.type}
+        </Text>
         <View
-          style={{
-            backgroundColor: item.bgColor,
-            justifyContent: "flex-end",
-            marginTop: 8,
-            flex: 1,
-            borderRadius: 10,
-            marginRight: 20,
-            padding: 10,
-          }}
+          style={[
+            {
+              backgroundColor: item.bgColor,
+              justifyContent: "flex-end",
+              marginTop: 8,
+              flex: 1,
+              borderRadius: 10,
+              marginRight: 20,
+              padding: 10,
+            },
+            styles.trendingshadows,
+          ]}
         >
           <View
             style={{
@@ -69,22 +122,72 @@ function HomeScreen(props) {
               {item.price}
             </Text>
           </View>
+          <View
+            style={{
+              position: "absolute",
+              top: 0.1,
+              right: -90,
+              width: "95%",
+            }}
+          >
+            <Svg width={100} height={100}>
+              <Polygon fill="white" points="0,0 160,0 168,80" />
+            </Svg>
+          </View>
           <Image
             source={item.img}
             resizeMode="contain"
             style={{
               position: "absolute",
-              top: -20,
+              top: -30,
               right: -50,
               bottom: 0,
               height: 200,
               width: 200,
+              transform: [{ rotate: "-15deg" }],
             }}
           />
         </View>
       </TouchableOpacity>
     );
   };
+  const renderRecentlyViewed = (item, index) => {
+    return (
+      <TouchableOpacity
+        onPress={() => console.log(item, "pressed")}
+        style={{ flex: 1, flexDirection: "row", padding: 15 }}
+      >
+        <View
+          style={{
+            paddingleft: 5,
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={item.img}
+            resizeMode="contain"
+            style={{ width: 130, height: 120 }}
+          />
+        </View>
+        <View
+          style={{
+            alignItens: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "grey", fontSize: 15, fontWeight: "700" }}>
+            {item.name}
+          </Text>
+          <Text style={{ color: "black", fontSize: 15, fontWeight: "bold" }}>
+            {item.price}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.headersection}>
@@ -110,11 +213,61 @@ function HomeScreen(props) {
           renderItem={({ item, index }) => TrendingShoes(item, index)}
         />
       </View>
+
+      {/* lower half */}
+
+      <View
+        style={[
+          {
+            flex: 1,
+            flexDirection: "row",
+            marginTop: 30,
+            borderTopLeftRadius: 35,
+            borderTopRightRadius: 35,
+          },
+          styles.lowerhalfcontainer,
+        ]}
+      >
+        <View style={{ width: 70 }}>
+          <Image
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+            source={require("../../assets/recently-viewed-label.png")}
+          />
+        </View>
+        <View style={{ flex: 1, padding: 24 }}>
+          <FlatList
+            data={recentlyViewed}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item, index }) => renderRecentlyViewed(item, index)}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  lowerhalfcontainer: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.45,
+    shadowRadius: 0.51,
+    elevation: 5,
+  },
+  trendingshadows: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
   trendshoeslist: {
     height: 240,
     width: 240,
@@ -144,7 +297,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: "lightgrey",
   },
 });
 
